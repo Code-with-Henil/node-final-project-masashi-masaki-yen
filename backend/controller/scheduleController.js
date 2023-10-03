522e8ddb6093787af9d20b9a6a91bd4938795762
MasakiFukunishi <fmsknn@gmail.com>
2023-10-03 11:40:04 -0700

    Add sql connection and schema

--- backend/controller/scheduleController.js
+++ backend/controller/scheduleController.js
@@ -1,17 +1,21 @@
-import { Client } from "../helper/common.js";
+import { client } from "../helper/db.js";
+
+await client.connect();
 
 export const getAppointment = async (req, res) => {
-	res.send("getAppointment");
+  const { rows } = await client.query("SELECT * FROM appointment");
+  console.log(rows);
+  res.send("getAppointment");
 };
 
 export const pushAppointment = async (req, res) => {
-	res.send("pushAppointment");
+  res.send("pushAppointment");
 };
 
 export const putAppointment = async (req, res) => {
-	res.send("putAppointment");
+  res.send("putAppointment");
 };
 
 export const deleteAppointment = async (req, res) => {
-	res.send("deleteAppointment");
+  res.send("deleteAppointment");
 };
---
+++ backend/helper/db.js
@@ -0,0 +1,14 @@
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const { Client } = pg;
+
+export const client = new Client({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_USER_PASSWORD,
+  port: process.env.DB_PORT,
+});
