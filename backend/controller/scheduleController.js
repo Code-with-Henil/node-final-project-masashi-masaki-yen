1de1c50626fc6417fdebb5fb8a6c8565cfd42e8f
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-10-04 19:12:41 -0700

    set update interview

--- backend/controller/scheduleController.js
+++ backend/controller/scheduleController.js
@@ -1,19 +1,28 @@
 import AppointmentModel from "../models/appointmentModel.js";
-
+import ModifiedInterviewModel from "../models/modifyInterviewModel.js";
 export const getAppointment = async (req, res) => {
-  const appointments = await AppointmentModel.getAppointment();
-  console.log(appointments);
-  res.send("getAppointment");
+	const appointments = await AppointmentModel.getAppointment();
+	console.log(appointments);
+	res.send("getAppointment");
 };
 
 export const pushAppointment = async (req, res) => {
-  res.send("pushAppointment");
+	res.send("pushAppointment");
 };
 
 export const putAppointment = async (req, res) => {
-  res.send("putAppointment");
+	const { id } = req.params;
+	const { interviewee_name, interviewer_id, appointment_id } = req.body;
+	const modifyingAppointment = await ModifiedInterviewModel.putAppointment(
+		id,
+		interviewee_name,
+		interviewer_id,
+		appointment_id
+	);
+
+	console.log(modifyingAppointment);
 };
 
 export const deleteAppointment = async (req, res) => {
-  res.send("deleteAppointment");
+	res.send("deleteAppointment");
 };
