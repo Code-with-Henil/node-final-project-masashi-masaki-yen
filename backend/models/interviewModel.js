739f29d62f9960d56053b9a8a140e54cbbfa2c97
YenLun <yenlunliu@gmail.com>
2023-10-04 17:28:10 -0700

    pushAppointment not done

--- backend/models/interviewModel.js
+++ backend/models/interviewModel.js
@@ -1,11 +1,22 @@
 import { client } from "../helper/db.js";
 
 class InterviewModel {
-    async pushAppointment() {
-        const { rows } = await client.query(
-            "INSERT INTO appointment (interviewee_name)"
-        );
-        return rows;
+    static async pushAppointment(
+        id,
+        intervieweeName,
+        interviewerId,
+        appointmentId
+    ) {
+        try {
+            const insertQuery =
+                "INSERT INTO interview (id, interviewee_name, interviewer_id, appointment_id) VALUES ($1,$2,$3,$4)";
+            const values = [id, intervieweeName, interviewerId, appointmentId];
+            await client.query(insertQuery, values);
+            return "Interview added successfully";
+        } catch (err) {
+            console.log("sth wrong");
+            throw err;
+        }
     }
 }
 
--- backend/routes/scheduleRouter.js
+++ backend/routes/scheduleRouter.js
@@ -5,6 +5,7 @@ import {
     putAppointment,
     deleteAppointment,
 } from "../controller/scheduleController.js";
+import InterviewModel from "../models/interviewModel.js";
 
 const scheduleRouter = express.Router();
 
@@ -15,7 +16,21 @@ scheduleRouter.get("/", async (req, res) => {
 
 scheduleRouter.post("/", async (req, res) => {
     console.log("interviewTesting");
-    pushAppointment(req, res);
+    try {
+        const { id, intervieweeName, interviewerId, appointmentId } = req.body;
+        const interviews = await InterviewModel.pushAppointment(
+            id,
+            intervieweeName,
+            interviewerId,
+            appointmentId
+        );
+        console.log(interviews);
+        res.status(201).send("Appointment pushed successfully");
+    } catch (error) {
+        console.error("Error pushing appointment:", error);
+        res.status(500).send("Error pushing appointment");
+    }
+    // pushAppointment(req, res);
 });
 
 scheduleRouter.put("/:id", async (req, res) => {
