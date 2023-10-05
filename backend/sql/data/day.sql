7a151e64036517f698ff29dcae921f25421dc4eb
MasakiFukunishi <fmsknn@gmail.com>
2023-10-05 09:02:21 -0700

    add day.sql

---
+++ backend/sql/data/day.sql
@@ -0,0 +1,5 @@
+INSERT INTO days ( day_title ) VALUES ( 'monday' );
+INSERT INTO days ( day_title ) VALUES ( 'tuesday' );
+INSERT INTO days ( day_title ) VALUES ( 'wednesday' );
+INSERT INTO days ( day_title ) VALUES ( 'thursday' );
+INSERT INTO days ( day_title ) VALUES ( 'friday' );
---
+++ backend/sql/data/interviewer.sql
@@ -0,0 +1,14 @@
+INSERT INTO interviewer ( interviewer_name, interviewer_img )
+VALUES ( 'Sylvia Palmer', 'https://i.imgur.com/LpaY82x.png');
+
+INSERT INTO interviewer ( interviewer_name, interviewer_img )
+VALUES ('Tori Malcolm', 'https://i.imgur.com/Nmx0Qxo.png' );
+
+INSERT INTO interviewer ( interviewer_name, interviewer_img )
+VALUES ('Mildred Nazir', 'https://i.imgur.com/T2WwVfS.png');
+
+INSERT INTO interviewer ( interviewer_name, interviewer_img )
+VALUES ('Cohana Roy', 'https://i.imgur.com/FK8V841.jpg');
+
+INSERT INTO interviewer ( interviewer_name, interviewer_img )
+VALUES ('Sven Jones', 'https://i.imgur.com/twYrpay.jpg');
