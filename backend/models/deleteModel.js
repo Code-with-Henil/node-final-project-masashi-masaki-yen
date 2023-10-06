ecf9d2a574265ebd59a26da9b7cb998702eb2a67
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-10-06 10:19:12 -0700

    set delete appointment part2

--- backend/models/deleteModel.js
+++ backend/models/deleteModel.js
@@ -3,7 +3,7 @@ import { client } from "../helper/db.js";
 class DeleteModel {
   static async deleteAppointment(id) {
     try {
-      const { rows } = await client.query("DELETE FROM interview WHERE id = $1 RETURNING id", [id]);
+      const { rows } = await client.query("DELETE FROM interview WHERE appointment_id = $1 RETURNING id", [id]);
       return rows;
     } catch (error) {
       console.log("Error deleting");
