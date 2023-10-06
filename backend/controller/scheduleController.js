3c2ffaa364d633f0960fcdbffcccbc5bdb8ead70
YenLun <yenlunliu@gmail.com>
2023-10-06 09:11:05 -0700

    fix variable name in pushAppointment

--- backend/controller/scheduleController.js
+++ backend/controller/scheduleController.js
@@ -5,75 +5,91 @@ import InterviewModel from "../models/interviewModel.js";
 import AvaitableInterviewer from "../models/availableInterviewer.js";
 
 export const getAppointment = async (req, res) => {
-  const { day } = req.params;
-  const appointments = await AppointmentModel.getAppointment(day);
-  const formattedData = {};
+    const { day } = req.params;
+    const appointments = await AppointmentModel.getAppointment(day);
+    const formattedData = {};
 
-  appointments.forEach((item) => {
-    const id = item.appointment_id.toString();
-    const time = item.time;
+    appointments.forEach((item) => {
+        const id = item.appointment_id.toString();
+        const time = item.time;
 
-    formattedData[id] = { id: item.appointment_id, time };
+        formattedData[id] = { id: item.appointment_id, time };
 
-    if (item.interview_id !== null) {
-      formattedData[id].interview = {
-        id: item.interview_id,
-        student: item.interviewee_name,
-        interviewer: {
-          id: item.interviewer_id,
-          name: item.interviewer_name,
-          avatar: item.interviewer_img,
-        },
-      };
-    }
-  });
-  res.status(200).json(formattedData);
+        if (item.interview_id !== null) {
+            formattedData[id].interview = {
+                id: item.interview_id,
+                student: item.interviewee_name,
+                interviewer: {
+                    id: item.interviewer_id,
+                    name: item.interviewer_name,
+                    avatar: item.interviewer_img,
+                },
+            };
+        }
+    });
+    res.status(200).json(formattedData);
 };
 
 export const getAvaitableInterviewers = async (req, res) => {
-  const { day } = req.params;
-  const interviewers = await AvaitableInterviewer.get(day);
+    const { day } = req.params;
+    const interviewers = await AvaitableInterviewer.get(day);
 
-  const groupedData = {};
+    const groupedData = {};
 
-  interviewers.forEach((item) => {
-    const { appointment_id, available_interviewer_id, interviewer_id, interviewer_name, interviewer_img } = item;
+    interviewers.forEach((item) => {
+        const {
+            appointment_id,
+            available_interviewer_id,
+            interviewer_id,
+            interviewer_name,
+            interviewer_img,
+        } = item;
 
-    if (!groupedData[appointment_id]) {
-      groupedData[appointment_id] = [];
-    }
+        if (!groupedData[appointment_id]) {
+            groupedData[appointment_id] = [];
+        }
 
-    groupedData[appointment_id].push({
-      id: interviewer_id,
-      name: interviewer_name,
-      avatar: interviewer_img,
+        groupedData[appointment_id].push({
+            id: interviewer_id,
+            name: interviewer_name,
+            avatar: interviewer_img,
+        });
     });
-  });
-  res.status(200).json(groupedData);
+    res.status(200).json(groupedData);
 };
 
 export const pushAppointment = async (req, res) => {
-  try {
-    const { intervieweeName, interviewerId, appointmentId } = req.body;
-    const interviews = await InterviewModel.pushAppointment(intervieweeName, interviewerId, appointmentId);
-    console.log("interviews", interviews);
-    res.status(201).send("Appointment pushed successfully");
-  } catch (error) {
-    console.error("Error pushing appointment:", error);
-    res.status(500).send("Error pushing appointment");
-  }
+    try {
+        const { interviewee_name, interviewer_id, appointment_id } = req.body;
+        console.log("req.body", req.body);
+        const interviews = await InterviewModel.pushAppointment(
+            interviewee_name,
+            interviewer_id,
+            appointment_id
+        );
+        console.log("interviews", interviews);
+        res.status(201).send("Appointment pushed successfully");
+    } catch (error) {
+        console.error("Error pushing appointment:", error);
+        res.status(500).send("Error pushing appointment");
+    }
 };
 
 export const putAppointment = async (req, res) => {
-  const { id } = req.params;
-  const { interviewee_name, interviewer_id, appointment_id } = req.body;
-  const modifyingAppointment = await ModifiedInterviewModel.putAppointment(id, interviewee_name, interviewer_id, appointment_id);
+    const { id } = req.params;
+    const { interviewee_name, interviewer_id, appointment_id } = req.body;
+    const modifyingAppointment = await ModifiedInterviewModel.putAppointment(
+        id,
+        interviewee_name,
+        interviewer_id,
+        appointment_id
+    );
 
-  console.log(modifyingAppointment);
+    console.log(modifyingAppointment);
 };
 
 export const deleteAppointment = async (req, res) => {
-  const { id } = req.params;
-  const deleteAppointment = await DeleteModel.deleteAppointment(id);
-  console.log(deleteAppointment);
+    const { id } = req.params;
+    const deleteAppointment = await DeleteModel.deleteAppointment(id);
+    console.log(deleteAppointment);
 };
